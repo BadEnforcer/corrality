7953127943dfc9e16e85d5c0d7883fdfbfd30c36
Manas <manaspvt8@gmail.com>
2025-04-05 20:22:40 +0530

    setup

---
+++ portfolioo/src/app/page.js
@@ -0,0 +1,13 @@
+// import Image from "next/image";
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div className=''>
+      
+      Navbar
+    </div>
+  );
+}
+
+export default Page;
